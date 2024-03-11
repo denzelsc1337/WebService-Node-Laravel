0cbefbf80a14dddb518bdb36e8bb2a3f2109b0ca
denzelsc1337 <denzelsotomayor@gmail.com>
2024-03-11 12:15:25 -0500

    locations 2

--- src/controllers/cliente.controller.js
+++ src/controllers/cliente.controller.js
@@ -18,3 +18,23 @@ export const getLocales = async (req, res) =>{
     console.log(result);
     res.json(result.recordset);
 }
+
+
+export const getInfoLocation = async (req, res) =>{
+    console.log(req.params);
+    const pool = await getConnection();
+
+    const result = await pool
+    .request()
+    .input('id_cli', sql.Int, req.params.id_cli)
+    .execute("usp_webcli_test_Get_Local")
+
+    if(result.rowsAffected[0] === 0){
+        return res.status(400).json({
+            message: "Local no encontrado"
+        })
+    }
+
+    return res.json(result.recordset[0]);
+    // console.log(result);
+}
--- src/routes/cliente.routes.js
+++ src/routes/cliente.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
-import {getClientes, getLocales} from "../controllers/cliente.controller.js"
+import {getClientes, getLocales, getInfoLocation} from "../controllers/cliente.controller.js"
 
 const router = Router();
 
 router.get('/clientes', getClientes );
 router.get('/locales', getLocales );
+router.get('/location/:id_cli', getInfoLocation );
+
 
 
 export default router;
